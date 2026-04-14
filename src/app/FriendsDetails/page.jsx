5620c26dd04414ee7a3df8a41466b3e3807ab0a3
GrRabby <grrabby9@gmail.com>
2026-04-15 02:40:44 +0600

    Linked to friends details page

---
+++ src/app/FriendsDetails/page.jsx
@@ -0,0 +1,10 @@
+
+
+export default function FriendsDetails() {
+    return (
+        <main className="flex flex-col flex-1 items-center justify-center bg-[#F8FAFC] dark:bg-black">
+            <h1 className="text-3xl font-bold mb-4">Friends Details Page</h1>
+            <p className="text-lg text-gray-600 dark:text-gray-300">This is where you can view the details of your friends.</p>
+        </main>
+    );
+};
