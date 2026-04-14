3dc89399152fa7f6c67b8306ba7d55cca1145d03
GrRabby <grrabby9@gmail.com>
2026-04-14 22:36:06 +0600

    Added NavBar & Footer

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
             <h2 className="text-[48px] text-[#1F2937] font-bold">Friends to keep close in your life</h2>
             <p className="text-[#64748B] text-[16px]">Your personal shelf of meaningful connections. Browse, tend, and nurture the<br/>
 relationships that matter most.</p>
-            <button className="btn btn-primary text-white font-bold py-2 px-4 rounded flex items-center gap-0 mt-4">
+            <button className="btn btn-primary text-white font-bold py-4 px-4 rounded flex items-center gap-0 mt-4">
                 <IoMdAdd className="mr-2" />
                 Add a Friend
             </button>
