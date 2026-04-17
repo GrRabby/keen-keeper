d1fbfdc35e05b6f062d8a18954288e46f313a077
GrRabby <grrabby9@gmail.com>
2026-04-17 13:59:22 +0600

    Final Update

--- src/app/FriendsDetails/[FriendID]/loading.js
+++ src/app/FriendsDetails/[FriendID]/loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     
-    <main className="flex flex-col flex-1 items-center justify-center bg-[#F8FAFC] dark:bg-black px-10">
+    <main className="flex flex-col flex-1 items-center justify-center bg-[#F8FAFC] dark:bg-black p-20">
         <div className="flex w-52 flex-col gap-4">
         <div className="skeleton h-32 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
